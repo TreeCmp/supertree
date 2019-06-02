4a6133c1b05c8fef4eea1baa3cb1fc03adcb5585
goluch <goluch@eti.pg.edu.pl>
2019-06-02 21:21:56 +0200

    added phylotree and test empty projects

---
+++ phylotree/phylotree.h
@@ -0,0 +1,12 @@
+#ifndef PHYLO_TREE_H
+#define PHYLO_TREE_H
+
+/* phylo.h
+ * Definition of a phylogenetic tree and all basic phylogenetic tree operations.
+ */
+
+namespace PhyloTree
+{
+
+}
+#endif
